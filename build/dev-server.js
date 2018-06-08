dc6230ae56d2c91ba8c53a7a48c9fbb542a46cbe
lij021 <lij021@foresealife.com>
2018-06-08 16:29:27 +0800

    aaa

--- build/dev-server.js
+++ build/dev-server.js
@@ -1,5 +1,6 @@
 require('./check-versions')()
-
+var models = require('./db')
+var mysql = require('mysql')
 var config = require('../config')
 if (!process.env.NODE_ENV) {
   process.env.NODE_ENV = JSON.parse(config.dev.env.NODE_ENV)
@@ -13,6 +14,9 @@ var proxyMiddleware = require('http-proxy-middleware')
 var webpackConfig = require('./webpack.dev.conf')
 var http = require('http')
 
+
+
+
 // default port where dev server listens for incoming traffic
 var port = process.env.PORT || config.dev.port
 // automatically open browser, if not set will be false
@@ -24,42 +28,71 @@ var proxyTable = config.dev.proxyTable
 var app = express()
 var apiRoutes = express.Router()
 apiRoutes.get('/channel', function(req, res) {
-  let channel = ''
-  let url = encodeURI('http://api.jisuapi.com/news/channel?appkey=ca05a06b9221f5d1')
-  let getChannel = new Promise((resolve, reject) => {
-    http.get(url, response => {
-      response.on('data', data => {
-        channel += data
-      })
-      response.on('end', () => {
-        resolve(channel)
-      })
-    })
-  })
-  getChannel.then(channel => {
-    channel = JSON.parse(channel)
-    res.json(channel)
-  })
+  // let channel = ''
+  // let url = encodeURI('http://api.jisuapi.com/news/channel?appkey=ca05a06b9221f5d1')
+  // let getChannel = new Promise((resolve, reject) => {
+  //   http.get(url, response => {
+  //     response.on('data', data => {
+  //       channel += data
+  //     })
+  //     response.on('end', () => {
+  //       resolve(channel)
+  //     })
+  //   })
+  // })
+  // getChannel.then(channel => {
+  //   channel = JSON.parse(channel)
+  //   console.log(channel)
+  //   res.json(channel)
+  // })
+  res.json( {status: '0', msg: 'ok', result:['区块链新闻','市场行情', '丑哥动态']})
 })
 apiRoutes.get('/channel/:item', function(req, res) {
   let item = req.params.item
   function findChannel(item) {
     return new Promise((resolve, reject) => {
       let findResult = ''
-      let url = encodeURI('http://api.jisuapi.com/news/get?channel='+item+'&start=0&num=30&appkey=ca05a06b9221f5d1')
-      http.get(url, response => {
-        response.on('data', data => {
-          findResult += data
-        })
-        response.on('end', () => {
-          resolve(findResult)
-        })
-      })
+      // console.log("######", item)
+      // let url = encodeURI('http://api.jisuapi.com/news/get?channel='+item+'&start=0&num=30&appkey=ca05a06b9221f5d1')
+      // console.log("######", url)
+      // http.get(url, response => {
+      //   response.on('data', data => {
+      //     findResult += data
+      //   })
+      //   response.on('end', () => {
+      //     resolve(findResult)
+      //   })
+      // })
+      let conn = mysql.createConnection(models.mysql)
+      conn.connect();
+      let result = {status: '0', msg: 'ok', result:{"channel":"区块链新闻", "num": "30", "list":[]}}
+      conn.query("SELECT * FROM test.bit_news order by url desc limit 30", function (error, results) {
+        if (error) throw error;
+        for (var i = 0; i < results.length; i++)
+        {
+          let item = {}
+          item['title'] = results[i]['title'];
+          item['time'] = results[i]['time'];
+          item['src'] = '';
+          item['category'] = '';
+          item['pic'] ='';
+          item['content'] = results[i]['content'];
+          console.log(item);
+          result.result.list.push(item)
+        }
+        console.log(result);
+        resolve(result);
+      });
+
+      conn.end();
+
     })
   }
   findChannel(item)
     .then(findResult => {
-      res.json(JSON.parse(findResult))
+      console.log("I am here")
+      // res.json(JSON.parse(findResult))
+      res.json(findResult)
     })
 })
 apiRoutes.get('/search/:keywords', function(req, res) {
